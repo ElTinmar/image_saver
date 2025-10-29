e1d13e70b3cafe1a4624a21fb9e3f34208e68b17
ElTinmar <martin.privat@hotmail.fr>
2025-10-29 19:22:05 +0100

    headless

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from distutils.core import setup
 
 setup(
     name='image_saver',
+    python_requires='>=3.8',
     author='Martin Privat',
-    version='0.1.0',
+    version='0.1.1',
     packages=['image_saver','image_saver.tests'],
     license='MIT',
     description='write images to raw files, image files or videos',
     long_description=open('README.md').read(),
     install_requires=[
         "numpy", 
-        "opencv-python",
+        "opencv-python-headless",
     ]
 )
